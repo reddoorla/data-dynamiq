bdcda0115dd669b1f088d5a491ce853610d6c87c
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2024-09-13 15:28:47 -0700

    fix fetch?

--- src/routes/[[preview=preview]]/+page.svelte
+++ src/routes/[[preview=preview]]/+page.svelte
@@ -109,7 +109,7 @@ const handleSubmit = (event:any) => {
  });
 
 
- fetch("/contact", {
+ fetch("/", {
    method: "POST",
    headers: { "Content-Type": "application/x-www-form-urlencoded" },
    body: new URLSearchParams(formDataObject).toString(),
